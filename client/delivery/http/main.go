1bb3bc731c93cec25ad07c29b1afa4b20f104f5b
natnaelawel <natnael.awel@gmail.com>
2019-12-31 15:08:13 +0300

    project structure

--- client/delivery/http/main.go
+++ client/delivery/http/main.go
@@ -3,20 +3,9 @@ package main
 import (
 	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/TenaHub/client/delivery/http/handler"
-	"html/template"
 )
-var Templ *template.Template
 
-func init()  {
-	Templ = template.Must(template.ParseGlob("client/ui/templates/*"))
-}
 func main()  {
-
-	AdminHomeHandler := clientHandler.NewHomeHandler(Templ)
 	router := mux.NewRouter()
-	router.PathPrefix("/assets/").
-		Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("client/ui/assets"))))
-	router.HandleFunc("/", AdminHomeHandler.HomePage).Methods("GET")
-	http.ListenAndServe(":8282", router)
+	http.ListenAndServe(":8181", router)
 }
